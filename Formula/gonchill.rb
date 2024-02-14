591560515e3c5712319726fc97b467da4b677550
kbwhodat <kato.byantalo@aiazing.com>
2024-02-13 21:24:24 -0500

    removing space

--- Formula/gonchill.rb
+++ Formula/gonchill.rb
@@ -2,7 +2,7 @@ class Gonchill < Formula
   desc "Your Go CLI app that allows you to stream via torrents, with quickness"
   homepage "https://github.com/username/gonchill"
   url "https://github.com/kbwhodat/gonchill/releases/download/1.0.4/gonchill-1.0.4.tar.gz"
-  sha256 "fdace76053f5ae2990b79a789b6a1caa6cb7ddcaa26371c313b4cb927865a0f0 "
+  sha256 "fdace76053f5ae2990b79a789b6a1caa6cb7ddcaa26371c313b4cb927865a0f0"
   license "MIT"
 
   depends_on "go" => :build
